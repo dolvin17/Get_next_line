ec6d76a20159b0486d4e9a57f3be04ace0de121b
ghuertas <ghuertas@student.42madrid.com>
2022-06-03 19:17:52 +0200

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,66 +6,30 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 12:04:58 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/06/03 17:33:36 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/06/03 19:06:41 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int	read_buffer(int fd, char **storage)
-{
-	char	*aux;
-	char	*buffer;
-	int		bytes;
-
-	if (BUFFER_SIZE > 0)
-	{
-		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buffer)
-			return (NULL);
-	}
-	bytes = read(fd, buffer, BUFFER_SIZE);
-	if (bytes < 0 || buffer == NULL)
-		return (-1);
-	buffer[bytes] = '\0';
-	aux = ft_strjoin(*storage, buffer);
-	free(*storage);
-	*storage = aux;
-	free(buffer);
-	buffer = NULL;
-	return (bytes);
-}
-/*
-void	split_storage(char **storage, char **line)
-{
-	size_t	len;
-	char	*aux;
-
-	len = ft_strchr(*storage, '\n') - *storage;
-	*line = ft_substr(*storage, 0, len);
-	aux = ft_substr(*storage, len + 1, ft_strlen(*storage) - len);
-	free(*storage);
-	*storage = aux;
-}*/
+#include <fcntl.h>
+#include <unistd.h>
 
 int	main(void)
 {
-	FILE	*fp;
-	char	*line;
 	int		fd;
-	int		n_linea;
+	void	*buffer;
+	int		i;
 
-	n_linea = 0;
-	fp = fopen("test.txt", "r");
-	fd = fileno(fp);
-	while (1)
+	i = 0;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	read(fd, buffer, 100);
+	while (buffer)
 	{
-		line = get_next_line(fd);
-		if (line == NULL)
-			break ;
-		printf("La linea es %d: %s", n_linea, line);
-		free(line);
-		n_linea++;
+		write(1, buffer, 1);
+		i++;
 	}
+	close(fd);
 	return (0);
 }
