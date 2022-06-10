bb9d313de7241f33ffac5536ef4023dac476b7f3
dolvin17 <huertas876@gmail.com>
2022-06-10 17:14:45 +0200

    125%

---
+++ get_next_line_bonus.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 17:00:27 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/06/10 17:00:29 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>	/* Malloc & Free functions */
+# include <unistd.h> 	/* Write & Read functions */
+# include <stdio.h>		/* BUFSIZ definition*/
+# include <fcntl.h> 	/* Open function */
+# include <limits.h>	/* Macro defined OPEN_MAX */
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE BUFSIZ
+# endif
+
+# if BUFFER_SIZE > 9223372036854775806L /*Double long*/
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+/* libft functions */
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *str, int c);
+void	*ft_calloc(size_t count, size_t size);
+/* new functions */
+char	*get_before_jump(const char *str);
+char	*get_after_jump(const char *str);
+void	read_line(int fd, char **storage, char **tempo);
+char	*split_and_swap(char **storage, char **tempo);
+void	ft_free_strs(char **str, char **str1, char **str2);
+char	*get_next_line(int fd);
+
+#endif
