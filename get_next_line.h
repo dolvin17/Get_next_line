5c95879579bed58023a42f589700736ac8033847
dolvin17 <huertas876@gmail.com>
2022-06-10 17:13:58 +0200

    125%

--- get_next_line.h
+++ get_next_line.h
@@ -3,36 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 03:17:27 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/06/03 17:33:34 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/06/10 16:57:53 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>	/* Malloc & Free functions */
+# include <unistd.h> 	/* Write & Read functions */
+# include <stdio.h>		/* BUFSIZ definition*/
+# include <fcntl.h> 	/* Open function */
+# include <limits.h>	/* Macro defined OPEN_MAX */
+
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE BUFSIZ
 # endif
 
-# ifndef OPEN_MAX
-#  define OPEN_MAX 256
+# if BUFFER_SIZE > 9223372036854775806L /*Double long*/
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
 # endif
 
-# include <stdlib.h>	/* Malloc, free functions */
-# include <unistd.h> 	/* Write function */
-# include <stdio.h>
-# include <fcntl.h> 	/* Read function */
-# include <limits.h>	/* Macro defined OPEN_MAX */
-
 /* libft functions */
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int __c);
-
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *str, int c);
+void	*ft_calloc(size_t count, size_t size);
 /* new functions */
+char	*get_before_jump(const char *str);
+char	*get_after_jump(const char *str);
+void	read_line(int fd, char **storage, char **tempo);
+char	*split_and_swap(char **storage, char **tempo);
+void	ft_free_strs(char **str, char **str1, char **str2);
 char	*get_next_line(int fd);
-int		read_buffer(int fd, char **storage);
+
 #endif
